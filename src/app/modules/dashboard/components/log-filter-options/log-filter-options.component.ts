d7ce48390a3ce36456722b7ec42a203b5421cca2
Jonas Berger <knotenbaer@gmail.com>
2020-01-05 15:41:02 +0100

    log filters

--- src/app/modules/dashboard/components/log-filter-options/log-filter-options.component.ts
+++ src/app/modules/dashboard/components/log-filter-options/log-filter-options.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-log-filter-options',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogFilterOptionsComponent implements OnInit {
 
-  constructor() { }
+    operatorOptions: any[] = [
+        { name: "==", value: "==" },
+        { name: "<", value: "<" },
+        { name: ">", value: ">" },
+    ];
 
-  ngOnInit() {
-  }
+    filterForm : FormGroup = new FormGroup({
+		field: new FormControl(null, Validators.required),
+		operator: new FormControl(null, Validators.required),
+		value: new FormControl(null, Validators.required)
+	})
+
+    constructor() { }
+
+    ngOnInit() {
+    }
 
 }
